c3377e2ebf7adeba93797ed2deddc34ff6e63401
Christian Nielsen <120013949+HrSejer@users.noreply.github.com>
2024-04-15 12:14:17 +0200

    Lavet det hele, Vælg 1-4

--- ExceptionExercise/Program.cs
+++ ExceptionExercise/Program.cs
@@ -10,7 +10,18 @@ namespace ExceptionExercise
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Vælg et tal mellem 1-4");
+            int UserInput = Convert.ToInt32(Console.ReadLine());
+            if (UserInput > 0 && UserInput < 5)
+            {
+                Console.WriteLine("Meget godt");
+            }
+            else
+            {
+                Console.WriteLine("Du er dum");
+            }
 
+            Console.ReadLine();
         }
     }
 }
